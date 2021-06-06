c3e4981dfc6e9c5df003932d54034784fe860772
Chris Bayne <chriss.bayne@gmail.com>
2021-06-06 13:44:59 -0700

    Adding comments

--- duckfood/src/App.js
+++ duckfood/src/App.js
@@ -6,10 +6,11 @@ const App = () => {
 
   const [users, setUsers] = useState(userDataList)
 
-  // By using setUsers, the addUser function puts an object containing a 
+  // By using setUsers, the addData function puts an object containing a 
   // new user into the users array which is being passed to the AddUser component.
+  // A new ID is set based on the current number (+ 1) of user data sets each time a data set is submitted 
   // The spread operator keeps the current user array the same.
-  const addUser = user => {
+  const addData = user => {
     user.id = users.length + 1
     setUsers([...users, user])
   }
