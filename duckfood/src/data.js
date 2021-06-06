0c3de7dc5fb642e04987907362f3bf30cd7dd7e3
Chris Bayne <chriss.bayne@gmail.com>
2021-06-06 12:38:43 -0700

    Added user array with data objects

---
+++ duckfood/src/data.js
@@ -0,0 +1,31 @@
+const userDataList = [
+    {
+        id: 1,
+        name: 'John',
+        time: '07:00',
+        foodType: 'Poultry Starter Pellets',
+        location: 'Berlin, GER',
+        numberOfDucks: '30',
+        amount: '2 lbs'
+    },
+    {
+        id: 2,
+        name: 'Lisa',
+        time: '16:00',
+        foodType: 'Whea/Barley mix',
+        location: 'Kyoto, JP',
+        numberOfDucks: '45',
+        amount: '3 lbs'
+    },
+    {
+        id: 3,
+        name: 'Kristin',
+        time: '13:30',
+        foodType: 'Brown rice',
+        location: 'Victoria, BC',
+        numberOfDucks: '25',
+        amount: '2.5 lbs'
+    },
+]
+
+export default userDataList
