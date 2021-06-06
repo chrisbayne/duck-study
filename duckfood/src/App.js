cd5ccebf2bc4627e7c47c866af8f3793f3abf106
Chris Bayne <chriss.bayne@gmail.com>
2021-06-05 21:21:19 -0700

    Converting App.js into function component

--- duckfood/src/App.js
+++ duckfood/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>A Review of Duck Nutrition</h1>
+      <div className="row">
+        <div className="five columns">
+          <h2>Submit Data</h2>
+        </div>
+        <div className="seven columns">
+          <h2>View Data</h2>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
